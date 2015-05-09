d2a2f9a3dda365cc62ece8612e007c267f3944d7
Jackon Yang <jiekunyang@gmail.com>
2015-05-09 21:38:41 +0800

    grab asian of all match

--- crawler/grab.py
+++ crawler/grab.py
@@ -1,6 +1,6 @@
 # -*- Encoding: utf-8 -*-
 from crawler import download
-from crawler.models import Total
+from crawler.models import Total, D_Asian
 
 
 bf_cols = (
@@ -22,6 +22,21 @@ bf_cols = (
         ("notes", 30),
         )
 
+detail_cols = (
+        ("company", 0),
+        ("start_home", 2),
+        ("start_handicap", 3),
+        ("start_visiting", 4),
+        ("real_home", 5),
+        ("real_handicap", 6),
+        ("real_visiting", 7),
+        ("end_home", 8),
+        ("end_handicap", 9),
+        ("end_visiting", 10),
+        )
+
+
+
 
 def get_match_list():
     querysetlist=[]
@@ -31,5 +46,23 @@ def get_match_list():
     Total.objects.bulk_create(querysetlist)
 
 
+def get_match_asian(match_id):
+    querysetlist=[]
+    for data in download.asian(match_id):
+        kwargs = {k: data[idx] for k, idx in detail_cols}
+        querysetlist.append(D_Asian(match_id=match_id, **kwargs))
+    D_Asian.objects.bulk_create(querysetlist)
+
+
 if __name__ == '__main__':
     get_match_list()
+
+    i = 0
+    for match in Total.objects.all():
+        try:
+            get_match_asian(match.match_id)
+        except:
+            print 'error! match ID: %s' %  match.match_id
+        else:
+            i += 1
+            print i
--- crawler/models.py
+++ crawler/models.py
@@ -27,3 +27,25 @@ class Total(models.Model):
 
     def __unicode__(self):
         return '%s(%s.vs.%s)' % (self.match_id, self.name_home, self.name_visiting)
+
+
+class D_Asian(models.Model):
+    match_id = models.CharField(u'比赛 ID', max_length=32)
+    company = models.CharField(u'足彩公司', max_length=32)  # col-0
+
+    start_home = models.CharField(u'初盘-主队', max_length=8)  # col-2
+    start_handicap = models.CharField(u'初盘-盘口', max_length=8)  # col-3
+    start_visiting = models.CharField(u'初盘-客队', max_length=8)  # col-4
+
+    real_home = models.CharField(u'即时-主队', max_length=8)  # col-5
+    real_handicap = models.CharField(u'即时-盘口', max_length=8)  # col-6
+    real_visiting = models.CharField(u'即时-客队', max_length=8)  # col-7
+
+    end_home = models.CharField(u'终盘-主队', max_length=8)  # col-8
+    end_handicap = models.CharField(u'终盘-盘口', max_length=8)  # col-9
+    end_visiting = models.CharField(u'终盘-客队', max_length=8)  # col-10
+
+    timestamp = models.TimeField(auto_now=True)
+
+    def __unicode__(self):
+        return '%s(Asian-%s)' % (self.match_id, self.company)
