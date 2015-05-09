30181c6270faf894022cef835102c9753231f546
Jackon Yang <jiekunyang@gmail.com>
2015-05-09 22:40:20 +0800

    show more cols in admin

--- crawler/admin.py
+++ crawler/admin.py
@@ -5,13 +5,13 @@ from crawler.models import Total, D_Asian
 
 class TotalAdmin(admin.ModelAdmin):
     list_display = (
-        "match_id",
-        "name_home",
-        "t_basepoint",
-        "goal_home_full",
-        "goal_home_half",
-        "red_home",
-        "yellow_home",
+        "match_id", "League",
+        "name_home", "name_visiting",
+        "t_basepoint", "match_status",
+        "goal_home_full", "goal_visiting_full",
+        "goal_home_half", "goal_visiting_half",
+        "red_home", "red_visiting",
+        "yellow_home", "yellow_visiting",
         "is_betting", "notes", 'timestamp')
 
 
@@ -20,7 +20,8 @@ class D_AsianAdmin(admin.ModelAdmin):
         "match_id", "company",
         "start_home", "start_handicap", "start_visiting",
         "real_home", "real_handicap", "real_visiting",
-        "end_home", "end_handicap", "end_visiting"
+        "end_home", "end_handicap", "end_visiting",
+        "timestamp",
         )
 
 
