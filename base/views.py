b37e45b16992a37449d067993f7f5ff8d02f4174
Jackon Yang <jiekunyang@gmail.com>
2015-06-20 02:30:13 +0800

    match list

--- base/views.py
+++ base/views.py
@@ -2,6 +2,10 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+# used for http redirect
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
 
 def home(request):
-    return render_to_response('home.html', RequestContext(request))
+    return HttpResponseRedirect(reverse('match_list'))
--- matches/views.py
+++ matches/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render_to_response
 
 
 def match_list(request):
-    return render_to_response('home.html', RequestContext(request))
+    return render_to_response('match-list.html', RequestContext(request))
