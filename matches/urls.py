b280ba7e12d19e1c264fb6602df41d8ed6616550
Jackon Yang <jiekunyang@gmail.com>
2015-06-21 17:52:35 +0800

    disp all europe history in web page

--- matches/urls.py
+++ matches/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('matches.views',
     url(r'^$', 'match_list', name='match_list'),
+    url(r'^europe/(\d+)$', 'europe', name='europe'),
 )
--- matches/views.py
+++ matches/views.py
@@ -3,12 +3,19 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 
-from crawler.download import get_match_list
+import crawler.download as downloader
 
 def match_list(request):
 
     content = {
-            'match_list': get_match_list(),
+            'match_list': downloader.get_match_list(),
             }
 
     return render_to_response('match-list.html', RequestContext(request, content))
+
+def europe(request, match_id):
+    content = {
+            'match_id': match_id,
+            'history': downloader.europe(match_id),
+            }
+    return render_to_response('europe-history.html', RequestContext(request, content))
