6a18127efcdd89c3b591265922af6a220e78c775
Jackon Yang <jiekunyang@gmail.com>
2015-06-21 18:40:43 +0800

    disp without company name

--- matches/views.py
+++ matches/views.py
@@ -17,5 +17,11 @@ def europe(request, match_id):
     content = {
             'match_id': match_id,
             'history': downloader.europe(match_id),
+            'companys': {
+                '48408691': u'bet 365(英国)',
+                '48417881': u'Redbet(马耳他)',
+                '48407436': u'SB(Crown)',
+                '48424205': u'澳门',
+                },
             }
     return render_to_response('europe-history.html', RequestContext(request, content))
