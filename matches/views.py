d8d209a891c2f78e4814280b43bd7c0f9c121c19
Jackon Yang <jiekunyang@gmail.com>
2015-06-23 11:26:08 +0800

    download once a day

--- matches/views.py
+++ matches/views.py
@@ -14,18 +14,22 @@ def gen_match_date():
 def match_list(request):
 
     match_date = gen_match_date()
-    print match_date
 
-    # download and save into database
-    querysetlist=[]
-    for data in downloader.get_match_list():
-        kwargs = {k: v for k, v in data.items()}
-        kwargs['match_date'] = match_date
-        querysetlist.append(Match(**kwargs))
-    Match.objects.bulk_create(querysetlist)
+    match_list = Match.objects.filter(match_date=match_date)
+
+    if not match_list:  # download and save into database
+        querysetlist=[]
+        for data in downloader.get_match_list():
+            kwargs = {k: v for k, v in data.items()}
+            kwargs['match_date'] = match_date
+            querysetlist.append(Match(**kwargs))
+        Match.objects.bulk_create(querysetlist)
+
+        # query again
+        match_list = Match.objects.filter(match_date=match_date)
 
     content = {
-            'match_list': Match.objects.filter(match_date=match_date),
+            'match_list': match_list,
             }
 
     return render_to_response('match-list.html', RequestContext(request, content))
